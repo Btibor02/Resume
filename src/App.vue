da5773ff976beef9c06567ec8bed66fa2a3f262f
BlascsokTibor <136988961+Btibor02@users.noreply.github.com>
2023-06-19 23:31:53 +0200

    About me

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,59 @@
 <script setup lang="ts">
-  import { useI18n } from "vue-i18n";
-  import router from "src/router";
-  import { onMounted, ref } from "vue";
-  // import { Cookies } from "quasar";
+import { useI18n } from "vue-i18n";
+import router from "src/router";
+import { onMounted, ref } from "vue";
+// import { Cookies } from "quasar";
 
-  const leftDrawer = ref<boolean>(true);
+const leftDrawer = ref<boolean>(true);
 
-  let { locale, t } = useI18n();
+let { locale, t } = useI18n();
 
-  onMounted(() => {
-  });
+onMounted(() => {});
 
-  // Watch browser or browser page is close
-  window.addEventListener(
-    "beforeunload",
-    () => {
+// Watch browser or browser page is close
+window.addEventListener("beforeunload", () => {}, false);
+
+function menuItems() {
+  return [
+    {
+      icon: "mdi-home",
+      text: t("startPage"),
+      name: "startPage",
+      route: "/",
+      disabled: false,
+      separator: false,
     },
-    false
-  );
+  ];
+}
 
-  function menuItems() {
-    return [
-      {
-        icon: "mdi-home",
-        text: t("startPage"),
-        name: "startPage",
-        route: "/",
-        disabled: false,
-        separator: false,
-      },
-    ];
-  }
-
-  function toggleLanguage() {
-    locale.value = locale.value == "hu" ? "en" : "hu";
-  }
+function toggleLanguage() {
+  locale.value = locale.value == "hu" ? "en" : "hu";
+}
 </script>
 
 <template>
   <div class="q-pa-md">
     <q-layout view="hHh Lpr fFf">
-      <q-header class="bg-primary text-white text-left" elevated reveal>
+      <q-header style="background-color: #1f4987;" class="text-white text-left" elevated reveal>
         <q-toolbar>
-          <q-btn dense flat icon="mdi-menu" round @click="leftDrawer = !leftDrawer" />
-          <q-toolbar-title class="my-title" style="cursor: pointer" @click="router.push({ path: '/' })">
+          <q-btn
+            dense
+            flat
+            icon="mdi-menu"
+            round
+            @click="leftDrawer = !leftDrawer"
+          />
+          <q-toolbar-title
+            class="my-title"
+            style="cursor: pointer"
+            @click="router.push({ path: '/' })"
+          >
             Tibor Blascsok Resume
           </q-toolbar-title>
           <q-btn flat icon="mdi-comment-text-multiple" @click="toggleLanguage">
             <q-badge color="red" floating :label="locale" />
           </q-btn>
           <q-btn flat icon="mdi-theme-light-dark" @click="$q.dark.toggle" />
-          <q-btn dense flat icon="mdi-menu" round @click="leftDrawer = !leftDrawer" />
         </q-toolbar>
       </q-header>
 
@@ -66,7 +69,11 @@
           <!-- routes: -->
           <q-list>
             <template v-for="(menuItem, index) in menuItems()" :key="index">
-              <q-item clickable :disable="menuItem.disabled" :to="menuItem.route">
+              <q-item
+                clickable
+                :disable="menuItem.disabled"
+                :to="menuItem.route"
+              >
                 <q-item-section avatar>
                   <q-icon :name="menuItem.icon" />
                 </q-item-section>
@@ -81,11 +88,9 @@
         </q-scroll-area>
       </q-drawer>
 
-      <q-footer elevated reveal>
+      <q-footer style="background-color: #1f4987;" elevated reveal>
         <q-toolbar>
-          <q-toolbar-title class="text-center my-title">
-            Vite-Quasar minimal {{ $t("template") }} 2022 -
-          </q-toolbar-title>
+          <q-toolbar-title class="text-center my-title"> </q-toolbar-title>
         </q-toolbar>
       </q-footer>
 
@@ -101,26 +106,26 @@
 </template>
 
 <style lang="scss">
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s ease;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
 
-  .fade-enter-from,
-  .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 
-  .my-title {
-    font-size: 10px;
-    @media (min-width: 400px) {
-      font-size: calc(10px + 0.5vw);
-    }
-    @media (min-width: 800px) {
-      font-size: calc(14px + 0.5vw);
-    }
-    @media (min-width: 1200px) {
-      font-size: calc(18px + 0.5vw);
-    }
+.my-title {
+  font-size: 10px;
+  @media (min-width: 400px) {
+    font-size: calc(10px + 0.5vw);
   }
+  @media (min-width: 800px) {
+    font-size: calc(14px + 0.5vw);
+  }
+  @media (min-width: 1200px) {
+    font-size: calc(18px + 0.5vw);
+  }
+}
 </style>
--- src/views/StartPageView.vue
+++ src/views/StartPageView.vue
@@ -2,8 +2,66 @@
 
 <template>
   <q-page>
-    <div class="d-flex flex-column text-center justify-center" style="height: 90vh">
-      <h1>Kár hogy nincs ma cs</h1>
+    <div
+      class="d-flex flex-column text-center justify-center"
+      style="height: 90vh"
+    >
+      <div class="row">
+        <p style="font-size: 5em">{{ $t("aboutMe") }}</p>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <div class="column items-start justify-start">
+            <img
+              src="../assets/portre.jpg"
+              style="height: 40em; max-width: 30em"
+            />
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="row">
+            <div class="col-8">
+              <div class="column items-start">
+                <div class="col">
+                  <h5>{{ $t("name") }}</h5>
+                </div>
+                <div class="col">
+                  <h5>tiborblascsok02@gmail.com</h5>
+                </div>
+                <div class="col">
+                  <h5>Telefonszám</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div style="text-align: start">
+                <div class="col">
+                  <h5>{{ $t("skills") }}</h5>
+                </div>
+                <div class="col">
+                  <h5>
+                    {{ $t("web") }}
+                    <br />
+                    C#, PHP, TypeScript {{ $t("prof") }}
+                    <br />
+                    MySQL, NoSQL {{ $t("prof") }}
+                    <br />
+                    Angular, Vue.js {{ $t("prof") }}
+                    <br />
+                    Node.jg, Laravel {{ $t("prof") }}
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+            <p style="font-size: medium; text-align: justify;"> {{ $t("aboutMsg") }} </p>
+          </div>
+      </div>
     </div>
   </q-page>
 </template>
